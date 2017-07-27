5b3b13e03e956c96cc57a0f03109250c37287d44
Chris <chris.davis5440@gmail.com>
2017-07-27 19:50:36 +0000

    Added bootstrap using bower

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,1 +1,15 @@
-//Node Project
+var express = require("express");
+var app = express();
+var port = process.env.PORT;
+
+app.use(express.static('public/bootstrap'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+app.listen(port, process.env.IP, () => {
+  console.log("App is running on Cloud9 port:", port);
+});
